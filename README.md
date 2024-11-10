bf657d4d1a728d452447c6d9aef1c0f3c7410bd7
Erlisja Kore <korelisa@yahoo.com>
2024-11-09 20:27:11 -0500

    added two event listeners, added a function to modify the style in response to user interaction

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # DOM_signUp_page_Validation
+
+## Requirement
+
+[x] Cache at least one element using selectElementById.
+[x] Cache at least one element using querySelector or querySelectorAll.
+[x] Use the parent-child-sibling relationship to navigate between elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.).
+[] Iterate over a collection of elements to accomplish some task.
+[] Create at least one element using createElement.
+[] Use appendChild and/or prepend to add new elements to the DOM.
+[] Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent
+[] Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties.
+[] Modify at least one attribute of an element in response to user interaction.
+[] Register at least two different event listeners and create the associated event handler functions.
+[] Use at least two Browser Object Model (BOM) properties or methods.
+[x] Include at least one form and/or input with HTML attribute validation.
+[] Include at least one form and/or input with DOM event-based validation. (This can be the same form or input as the one above, but should include event-based validation in addition to the HTML attribute validation.)
+
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,66 @@
- 
+// Date: 2021/05/24
+
+// use of getElementById
+myForm = document.getElementById("my-form");
+console.log(myForm);
+
+// use of querySelector
+const firstName = document.querySelector(".first-name");
+console.log(firstName);
+const lastName = document.querySelector(".last-name");
+console.log(lastName);
+const email = myForm.elements["email"];
+console.log(email);
+
+const password = myForm.elements["password"];
+const confirmPassword = myForm.elements["confirm-password"];
+console.log(confirmPassword);
+const usernameField = myForm.elements["username"];
+const stateField = myForm.elements["state"];
+const cityField = myForm.elements["city"];
+const zipField = myForm.elements["zip"];
+const checkField = myForm.elements["checkbox"];
+
+
+// selecting using parentElement and nextElementSibling 
+const parentElement = firstName.parentNode;
+console.log(parentElement);
+const siblingElement = parentElement.nextElementSibling;
+console.log(siblingElement);
+
+
+// use of addEventListener 1
+
+myForm.addEventListener("submit", function(event) {
+    if (password.value!== confirmPassword.value) {
+        event.preventDefault(); 
+        showCustomAlert('Passwords do not match');
+    }
+    
+    });
+
+// use of addEventListener 2
+function togglePassword(){
+    if (password.type === "password"){
+        password.type = "text";
+    } else {
+        password.type = "password";
+    }
+}
+function togglePassword1(){
+    if (confirmPassword.type === "password"){
+        confirmPassword.type = "text";
+    } else {
+        password.type = "password";
+    }
+}
+// add a border if the passwords do not match when typing
+confirmPassword.addEventListener("input", function(event){
+    if (password.value !== confirmPassword.value){
+        confirmPassword.style.border = "2px solid red";
+    } else {
+        confirmPassword.setCustomValidity("");
+        confirmPassword.style.border = "2px solid green";
+    }
+});
+
